e1ec25116245024e2eef290fb708b1d583f7f82f
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2022-01-22 13:20:15 +0700

    add datatables and sweetalert

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // tambahan
+        Yajra\DataTables\DataTablesServiceProvider::class,
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+
     ],
 
     /*
@@ -229,6 +233,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //tambahan
+        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
 
     ],
 
---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard SINDU - Sistem Informasi Hindu</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('asset/admin/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet"
+    href="{{ asset('asset/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ asset('asset/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{ asset('asset/admin/plugins/jqvmap/jqvmap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('asset/admin/dist/css/adminlte.min.css') }}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset('asset/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{ asset('asset/admin/plugins/daterangepicker/daterangepicker.css') }}">
+  <!-- summernote -->
+  {{-- <link rel="stylesheet" href="{{asset('asset/admin/plugins/summernote/summernote-bs4.min.css')}}"> --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+  <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+  @yield('scriptCss')
+  <link rel="stylesheet" href="{{ asset('asset/font/all.min.css') }}">
+</head>
+<script src="https://kit.fontawesome.com/f7e99e0495.js" crossorigin="anonymous"></script>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <!-- Preloader -->
+    {{-- <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div> --}}
+
+    <!-- Navbar -->
+    @include('admin.tools.navbar')
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    @include('admin.tools.aside')
+
+    @include('sweetalert::alert')
+    <!-- Content Wrapper. Contains page content -->
+    @yield('content')
+    <!-- /.content-wrapper -->
+    @include('admin.tools.footer')
+
+    <!-- Control Sidebar -->
+
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+  @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
+
+  <!-- jQuery -->
+  {{-- <script src="{{asset('asset/admin/plugins/jquery/jquery.min.js')}}"></script> --}}
+  <!-- jQuery UI 1.11.4 -->
+  {{-- <script src="{{asset('asset/admin/plugins/jquery-ui/jquery-ui.min.js')}}"></script> --}}
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  {{-- <script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script> --}}
+  <!-- Bootstrap 4 -->
+  <script src="{{ asset('asset/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+  <!-- overlayScrollbars -->
+  <script src="{{ asset('asset/admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+  <!-- AdminLTE App -->
+  <script src="{{ asset('asset/admin/dist/js/adminlte.js') }}"></script>
+  <!-- AdminLTE for demo purposes -->
+  {{-- <script src="{{asset('asset/admin/dist/js/demo.js')}}"></script> --}}
+
+  @yield('scriptJs')
+</body>
+
+</html>
