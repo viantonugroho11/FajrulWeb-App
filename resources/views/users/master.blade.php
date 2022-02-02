a835d4c3651f8ac9c51594b414fcc8cb983664de
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2022-02-02 16:43:21 +0700

    add master user

---
+++ resources/views/users/master.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Fajrul Islam</title>
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:500,600&amp;display=swap">
+    <link rel="stylesheet" href="./assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="./assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="./assets/css/styles.min.css">
+</head>
+
+<body style="background: url(&quot;/assets/img/bg-aurora-full.png?h=6e7d0ed6e480a49d3f54140d74d327d5&quot;);background-size: cover;font-family: Poppins, sans-serif;">
+    <div class="container" style="width: 1200px;margin-bottom: 9rem;">
+        <!-- Start: Navigation Clean -->
+        <nav class="navbar navbar-light navbar-expand-lg navigation-clean" style="padding: 0;font-family: Poppins, sans-serif;background: rgba(255,255,255,0);height: 90px;">
+            <div class="container"><img src="./assets/img/logo-fajrul-header.png" style="height: auto;width: 20%;"><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcol-1" style="font-weight: bold;font-size: 20px;">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link active" href="#" style="font-weight: bold;font-family: Poppins, sans-serif;">BERANDA</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" style="font-weight: bold;font-family: Poppins, sans-serif;">TENTANG</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" style="font-weight: bold;font-family: Poppins, sans-serif;">PROYEK</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" style="font-weight: bold;font-family: Poppins, sans-serif;">BLOG</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" style="font-weight: bold;font-family: Poppins, sans-serif;">ACARA</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav><!-- End: Navigation Clean -->
+        <div class="carousel slide" data-bs-ride="carousel" id="carousel-1">
+            <div class="carousel-inner">
+                <div class="carousel-item active"><img class="w-100 d-block" src="./assets/img/img-bg.png" alt="Slide Image"></div>
+                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/img-bg.png"></div>
+                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/img-bg.png"></div>
+            </div>
+            <div>
+                <!-- Start: Previous --><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><!-- End: Previous -->
+                <!-- Start: Next --><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a><!-- End: Next -->
+            </div>
+            <ol class="carousel-indicators">
+                <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
+                <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
+                <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
+            </ol>
+        </div>
+    </div><!-- Start: 1 Row 2 Columns -->
+    <div class="container" style="margin-bottom: 9rem;">
+        <div class="row">
+            <div class="col-md-6 align-self-center" style="padding: 0;text-align: center;"><img src="/assets/img/about-lp-img.png?h=daa792aaee5dee417b39dfbf0a598566" style="width: 80%;height: auto;"></div>
+            <div class="col-md-6 align-self-center" style="padding: 0;font-family: Poppins, sans-serif;background: rgba(255,255,255,0);">
+                <div class="card" style="border-style: none;background: rgba(255,255,255,0);">
+                    <div class="card-body" style="border-style: none;background: rgba(255,255,255,0);">
+                        <h4 class="card-title" style="border-style: none;font-family: Poppins, sans-serif;font-weight: bold;font-size: 44px;background: rgba(255,255,255,0);">Title</h4>
+                        <p class="card-text" style="border-style: none;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(47, 49, 61, 1);height: 50px;width: 210px;color: rgba(243, 243, 243, 1);border-radius: 30px;box-shadow: 0px 6px 20px rgba(47,49,61,0.25);border-style: none;">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End: 1 Row 2 Columns -->
+    <!-- Start: 1 Row 2 Columns -->
+    <div class="container" style="margin-bottom: 9rem;">
+        <div class="row">
+            <div class="col-md-6 align-self-center" style="text-align: center;padding: 0;">
+                <div class="card" style="border-style: none;background: rgba(255,255,255,0);">
+                    <div class="card-body" style="border-style: none;text-align: center;background: rgba(255,255,255,0);">
+                        <h4 class="card-title" style="border-style: none;text-align: center;font-size: 44px;font-family: Poppins, sans-serif;margin-bottom: 2rem;font-weight: bold;">10+ Program Kerja<br>Terealisasikan</h4><button class="btn btn-primary" type="button" style="background: rgba(47, 49, 61, 1);text-align: center;height: 50px;width: 210px;border-radius: 30px;border-style: none;box-shadow: 0px 6px 20px rgba(47, 49, 61, 0.25);">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 align-self-center" style="padding: 0;"><img src="/assets/img/proker-img.png?h=e8fdf7d7c8a9466a54334e46a39c8322" style="width: 80%;height: auto;"></div>
+        </div>
+    </div><!-- End: 1 Row 2 Columns -->
+    <!-- Start: 2 Rows 1+1 Columns -->
+    <div class="container" style="margin-bottom: 9rem;">
+        <div class="row">
+            <div class="col d-xxl-flex align-items-xxl-center" style="background: rgba(255,255,255,0);">
+                <div class="card" style="background: rgba(255,255,255,0);border-style: none;">
+                    <div class="card-body" style="background: rgba(255,255,255,0);">
+                        <h4 class="card-title" style="font-size: 44px;font-weight: bold;">BLOG</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-xxl-flex justify-content-xxl-end align-items-xxl-center">
+                <div class="card" style="border-style: none;background: rgba(255,255,255,0);">
+                    <div class="card-body" style="text-align: right;font-size: 23px;"><a class="card-link" href="#" style="color: rgb(2,20,45);font-family: Poppins, sans-serif;border-style: none;">More &gt;</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="row d-xxl-flex justify-content-xxl-center align-items-xxl-center" style="height: 600px;background: rgba(47, 49, 61, 1);border-radius: 20px;margin: 0px;box-shadow: 0px 15px 40px 0px rgba(33,37,41,0.5);">
+            <div class="col-md-12 text-start d-xxl-flex align-items-center align-self-center justify-content-xxl-center" style="margin: 0px;padding: 0px;">
+                <div class="card-group" style="width: 1200px;height: 500px;padding: 2%;color: rgba(243, 243, 243, 1);font-family: Poppins, sans-serif;">
+                    <div class="card d-xxl-flex" style="margin: 1rem;border-radius: 20px;background: rgba(243,243,243,0.2);">
+                        <div class="card-body" style="text-align: right;">
+                            <h4 class="card-title" style="text-align: left;background: rgba(243,243,243,0);font-size: 30px;">Title</h4>
+                            <p class="card-text" style="text-align: left;background: rgba(243,243,243,0);">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(13,110,253,0);border-style: none;">Button</button>
+                        </div>
+                    </div>
+                    <div class="card" style="margin: 1rem;width: 100%;height: auto;border-radius: 20px;background: rgba(243,243,243,0.2);">
+                        <div class="card-body" style="text-align: right;border-radius: 0;">
+                            <h4 class="card-title" style="text-align: left;font-family: rgba(243, 243, 243, 1);font-size: 30px;">Title</h4>
+                            <p class="card-text" style="text-align: left;font-family: rgba(243, 243, 243, 1);">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(13,110,253,0);border-style: none;">Button</button>
+                        </div>
+                    </div>
+                    <div class="card" style="margin: 1rem;border-radius: 20px;background: rgba(243,243,243,0.2);">
+                        <div class="card-body" style="text-align: right;">
+                            <h4 class="card-title" style="text-align: left;font-size: 30px;">Title</h4>
+                            <p class="card-text" style="text-align: left;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(13,110,253,0);border-style: none;">Button</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End: 2 Rows 1+1 Columns -->
+    <!-- Start: 2 Rows 1+1 Columns -->
+    <div class="container" style="margin-bottom: 9rem;">
+        <div class="row">
+            <div class="col  d-xxl-flex align-items-xxl-center" style="background: rgba(255,255,255,0);">
+                <div class="card" style="background: rgba(255,255,255,0);border-style: none;">
+                    <div class="card-body" style="background: rgba(255,255,255,0);">
+                        <h4 class="card-title" style="font-size: 44px;font-weight: bold;">ACARA</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-xxl-flex justify-content-xxl-end align-items-xxl-center">
+                <div class="card" style="border-style: none;background: rgba(255,255,255,0);">
+                    <div class="card-body" style="text-align: right;font-size: 23px;"><a class="card-link" href="#" style="color: rgb(2,20,45);font-family: Poppins, sans-serif;border-style: none;">More &gt;</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="row d-xxl-flex justify-content-xxl-center align-items-xxl-center" style="height: 600px;background: rgba(47, 49, 61, 1);border-radius: 20px;margin: 0px;box-shadow: 0px 15px 40px 0px rgba(33,37,41,0.5);">
+            <div class="col-md-12 text-start d-xxl-flex align-items-center align-self-center justify-content-xxl-center" style="margin: 0px;padding: 0px;">
+                <div class="card-group" style="width: 1200px;height: 500px;padding: 2%;color: rgba(243, 243, 243, 1);font-family: Poppins, sans-serif;">
+                    <div class="card d-xxl-flex" style="margin: 1rem;border-radius: 20px;background: rgba(243,243,243,0.2);">
+                        <div class="card-body" style="text-align: right;">
+                            <h4 class="card-title" style="text-align: left;background: rgba(243,243,243,0);font-size: 30px;">Title</h4>
+                            <p class="card-text" style="text-align: left;background: rgba(243,243,243,0);">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(13,110,253,0);border-style: none;">Button</button>
+                        </div>
+                    </div>
+                    <div class="card" style="margin: 1rem;width: 100%;height: auto;border-radius: 20px;background: rgba(243,243,243,0.2);">
+                        <div class="card-body" style="text-align: right;border-radius: 0;">
+                            <h4 class="card-title" style="text-align: left;font-family: rgba(243, 243, 243, 1);font-size: 30px;">Title</h4>
+                            <p class="card-text" style="text-align: left;font-family: rgba(243, 243, 243, 1);">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(13,110,253,0);border-style: none;">Button</button>
+                        </div>
+                    </div>
+                    <div class="card" style="margin: 1rem;border-radius: 20px;background: rgba(243,243,243,0.2);">
+                        <div class="card-body" style="text-align: right;">
+                            <h4 class="card-title" style="text-align: left;font-size: 30px;">Title</h4>
+                            <p class="card-text" style="text-align: left;">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary" type="button" style="background: rgba(13,110,253,0);border-style: none;">Button</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- End: 2 Rows 1+1 Columns -->
+    <div class="container" style="background: rgba(255,255,255,0);margin-bottom: 9rem;font-family: Poppins, sans-serif;">
+        <!-- Start: Newsletter Subscription Form -->
+        <section class="newsletter-subscribe" style="background: rgba(255,255,255,0);margin-bottom: 0px;">
+            <div class="container">
+                <!-- Start: Intro -->
+                <div class="intro" style="width: 1272px;">
+                    <h2 class="text-center" style="color: rgba(47, 49, 61, 1);font-size: 44px;">Tetap Terhubung dengan Kami</h2>
+                    <p class="text-center">agar kamu tetap update infromasi dari kami </p>
+                </div><!-- End: Intro -->
+                <form class="d-flex justify-content-center flex-wrap" method="post">
+                    <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Your Email" style="margin: 0px;border-radius: 10px;height: 53px;border-top-right-radius: 0;border-bottom-right-radius: 0;color: rgba(47, 49, 61, 1);border-style: solid;border-color: rgba(47, 49, 61, 1);border-top-color: rgba(47,;border-right-color: 49,;border-bottom-color: 61,;border-left-color: 1);"><button class="btn btn-primary" type="button" style="background: linear-gradient(rgba(47, 49, 61, 1) 100%, rgba(47, 49, 61, 1) 100%), rgba(47, 49, 61, 1);color: #f3f3f3;height: 53px;border-top-right-radius: 10px;border-bottom-right-radius: 10px;border-top-left-radius: 0px;border-bottom-left-radius: 0px;border-color: rgba(47, 49, 61, 1);border-top-color: rgba(47,;border-right-color: 49,;border-bottom-color: 61,;border-left-color: 1);">TERHUBUNG!</button></div>
+                </form>
+            </div>
+        </section><!-- End: Newsletter Subscription Form -->
+    </div><!-- Start: 1 Row 2 Columns -->
+    @include('users.tools.footer')
+    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
