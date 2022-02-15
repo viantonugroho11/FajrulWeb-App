0a62f6ba49a45e21c02657943143b9f5e26d8f03
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2022-02-15 13:47:28 +0700

    fixing dashbard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,7 +36,7 @@
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('artikel.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
