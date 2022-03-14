052f7db5841e9a946fed0974d7ab20406c78b8d2
viantonugroho <viantonugroho@gmail.com>
2022-03-14 12:27:53 +0700

    fixing content lading faris

---
+++ database/migrations/2022_03_14_022447_create_sertifikats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSertifikatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sertifikats', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sertifikats');
+    }
+}
--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -2,84 +2,56 @@
 
 @section('content')
 @include('frontend.tools.carosuel')
-  <!-- ***** About Us Area Start ***** -->
-  <section class="uza-about-us-area">
+<!-- ***** About Us Area Start ***** -->
+<div class="uza-about-us-area">
     <div class="container">
-      <div class="row align-items-center">
+        <div class="row align-items-center">
 
-        <!-- About Thumbnail -->
-        <div class="col-12 col-md-6">
-          <div class="about-us-thumbnail mb-80">
-            <img src="./img/bg-img/2.jpg" alt="">
-            <!-- Video Area -->
-            <div class="uza-video-area hi-icon-effect-8">
-              <a href="https://www.youtube.com/watch?v=sSakBz_eYzQ" class="hi-icon video-play-btn"><i class="fa fa-play"
-                  aria-hidden="true"></i></a>
+            <!-- About Thumbnail -->
+            <div class="col-12 col-md-6">
+                <div class="about-us-thumbnail mb-80">
+                    <img src="{{asset('assets/frontend/v1/imgfajrul/bg-img/img-aboutme.png')}}" alt="">
+                </div>
             </div>
-          </div>
-        </div>
 
-        <!-- About Us Content -->
-        <div class="col-12 col-md-6">
-          <div class="about-us-content mb-80">
-            <h2>Fajrul Islam</h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing esed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna.</p>
-            <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet gubergren no sea takimata
-              sanctus est Lorem ipsum dolor sit amet ipsumlor eut consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt labore et dolore magna
-              liquyam erat.</p>
-            <a href="#" class="btn uza-btn btn-2 mt-4">Start Exploring</a>
-          </div>
+            <!-- About Us Content -->
+            <div class="col-12 col-md-6">
+                <div class="about-us-content mb-80">
+                    <h2>Tentang Kami</h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut
+                        labore et
+                        dolore magna aliqua. Urna id volutpat lacus laoreet non curabitur gravida arcu ac. Purus
+                        faucibus ornare
+                        suspendisse sed nisi lacus sed viverra. Cursus in hac habitasse platea dictumst. Tellus
+                        molestie nunc
+                        non
+                        blandit massa enim nec dui nunc.</p>
+                    <a href="#" class="btn uza-btn btn-2 mt-4">Lihat selengkapnya</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
 
     <!-- About Background Pattern -->
     <div class="about-bg-pattern">
-      <img src="./img/core-img/curve-2.png" alt="">
+        <img src="{{asset('assets/frontend/v1/imgfajrul/img/core-img/curve-2.png')}}" alt="">
     </div>
-  </section>
-  <!-- ***** About Us Area End ***** -->
 
-  <!-- ***** About Us Area Start ***** -->
-  <section class="uza-about-us-area">
-    <div class="container">
-      <div class="row align-items-center">
+</div>
 
-        <!-- About Us Content -->
-        <div class="col-12 col-md-6">
-          <div class="about-us-content mb-80">
-            <h2>Fajrul Islam</h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing esed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna.</p>
-            <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet gubergren no sea takimata
-              sanctus est Lorem ipsum dolor sit amet ipsumlor eut consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt labore et dolore magna
-              liquyam erat.</p>
-            <a href="#" class="btn uza-btn btn-2 mt-4">Start Exploring</a>
-          </div>
-        </div>
-        <!-- About Thumbnail -->
-        <div class="col-12 col-md-6">
-          <div class="about-us-thumbnail mb-80">
-            <img src="./img/bg-img/2.jpg" alt="">
-            <!-- Video Area -->
-            <div class="uza-video-area hi-icon-effect-8">
-              <a href="https://www.youtube.com/watch?v=sSakBz_eYzQ" class="hi-icon video-play-btn"><i
-                  class="fa fa-play" aria-hidden="true"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
+<!-- program Kerja -->
+<div class="row d-flex proker">
+    <div class="col m-auto text-center">
+        <h3 class="tag-proker no-gutters">10+ Program Kerja <br> Terealisasikan</h3>
+        <a href="#" class="btn uza-btn btn-2 mt-4">Lihat selengkapnya</a>
     </div>
-    <!-- About Background Pattern -->
-    <div class="about-bg-pattern">
-      <img src="./img/core-img/curve-2.png" alt="">
+    <div class="col m-auto">
+        <img src="{{asset('assets/frontend/v1/imgfajrul/bg-img/program-kerja.png')}}" width="489px" height="475px">
     </div>
-  </section>
-  <!-- ***** About Us Area End ***** -->
+</div>
+<!-- End -->
 
-  @include('frontend.tools.blogLatest')
-  @include('frontend.tools.eventLatest')
+@include('frontend.tools.blogLatest')
+@include('frontend.tools.eventLatest')
 @endsection
