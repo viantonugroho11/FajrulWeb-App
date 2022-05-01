304f313faccd5d4df508d2948fec756f1f8b44c2
Rizky Sinpro <rizky.putra@sinproworks.com>
2022-05-01 23:12:30 +0700

    Update show detail

---
+++ resources/views/frontend/tentang/periode.blade.php
@@ -0,0 +1,32 @@
+@extends('frontend.master')
+
+@section('content')
+
+<section class="blog-details-area section-padding-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="blog-details-content">
+                    <!-- Post Details Text -->
+                    <div class="post-details-text">
+
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-lg-10">
+                                <div class="post-content text-center mb-50">
+                                    <h2>Sejarah</h2>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <img class="mb-50" src="img/bg-img/14.jpg" alt="">
+                            </div>
+                            <div class="col-12 col-lg-10">Sehubungan dengan dimulainya tahun akademik baru 2021/2022, UKM Fajrul Islam sebagai lembaga kemahasiswaan bernuansa Islam yang ada di lingkungan Universitas Gunadarma mengadakan Open Recruitment yang merupakan seleksi untuk menjaring sumber daya manusia yang berkualitas dan layak untuk diangkat menjadi anggota ataupun menduduki suatu jabatan tertentu dari organisasi.
+                                Tujuan kegiatan ini adalah merekrut mahasiswa yang berkomitmen di jalan dakwah, membangun budaya literasi Islam pada diri mahasiswa, membentuk karakter mahasiswa yang berakhlakul karimah, membuka kesempatan bagi mahasiswa untuk mengaktualisasikan diri, memaksimalkan potensi yang dimiliki setiap mahasiswa, memberikan manfaat kepada mahasiswa terkait peran organisasi.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/frontend/tentang/tentang.blade.php
@@ -0,0 +1,35 @@
+@extends('frontend.master')
+
+@section('content')
+
+<section class="blog-details-area section-padding-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="blog-details-content">
+                    <!-- Post Details Text -->
+                    <div class="post-details-text">
+
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-lg-10">
+                                <div class="post-content text-center mb-50">
+                                    <h2>Tentang Kami</h2>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <img class="mb-50" src="img/bg-img/14.jpg" alt="">
+                            </div>
+                            <div class="col-12 col-lg-10">
+                                UKM Fajrul Islam Universitas Gunadarma merupakan suatu organisasi yang berbasis keislaman dan
+                                keberadaannya resmi di bawah wakil rektor III Universitas Gunadarma. Salah satu tujuan Fajrul Islam sebagai
+                                lembaga dakwah kampus tidak lain adalah membumikan nilai-nilai Islam di kampus Universitas Gunadarma
+                                tercinta.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
